6599089c9edb51114094615a9f330c9f3a7f74fc
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2024-11-23 13:44:58 +0600

    research and project structure completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Question Answer Chatbot",
+    author="Amin",
+    author_email="Amincolab@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires = []
+)
